ae8c02a60cd35104a018f28765ad0fc074133027
粽子码 <96876694+DongZhizhen@users.noreply.github.com>
2022-01-07 21:24:18 +0800

    Add files via upload

---
+++ SapOptV1.0/solve.f
@@ -0,0 +1,13 @@
+
+/solu
+nsel,s,loc,z,-30 
+nsel,r,loc,x,25,33        
+d,all,uy,0   
+d,all,uz,0   
+
+omega,0,3654.7,0
+
+antype,static          
+eqslv,pcg         
+allsel,all                  
+solve
